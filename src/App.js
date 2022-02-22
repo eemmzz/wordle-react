5c097c195873f71bc28f19f7abde88ddd411b21a
Emma Young <emma.maloney@bbc.co.uk>
2022-02-22 22:18:21 +0000

    feat: add initial keyboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Grid from './components/Grid';
+import Keyboard from './components/Keyboard';
 
 function App() {
   return (
     <div className="App">
       <h1>Wordle React</h1>
       <Grid />
+      <Keyboard />
     </div>
   );
 }
