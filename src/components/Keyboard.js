5821a27d541d74b435859e0350e4294634d17590
Emma Young <emma.maloney@bbc.co.uk>
2022-02-22 22:31:40 +0000

    fix: resolve react warnings
    1. missing tbody
    2. class -> className

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,40 +1,42 @@
 const Keyboard = () => {
   return (
-    <table class="App_Keyboard">
-      <tr>
-        <td><button value="Q" class="App_Keyboard_Button">Q</button></td>
-        <td><button value="W" class="App_Keyboard_Button">W</button></td>
-        <td><button value="E" class="App_Keyboard_Button">E</button></td>
-        <td><button value="R" class="App_Keyboard_Button">R</button></td>
-        <td><button value="T" class="App_Keyboard_Button">T</button></td>
-        <td><button value="Y" class="App_Keyboard_Button">Y</button></td>
-        <td><button value="U" class="App_Keyboard_Button">U</button></td>
-        <td><button value="I" class="App_Keyboard_Button">I</button></td>
-        <td><button value="O" class="App_Keyboard_Button">O</button></td>
-        <td><button value="P" class="App_Keyboard_Button">P</button></td>
-      </tr>
-      <tr>
-        <td><button value="A" class="App_Keyboard_Button">A</button></td>
-        <td><button value="S" class="App_Keyboard_Button">S</button></td>
-        <td><button value="D" class="App_Keyboard_Button">D</button></td>
-        <td><button value="F" class="App_Keyboard_Button">F</button></td>
-        <td><button value="G" class="App_Keyboard_Button">G</button></td>
-        <td><button value="H" class="App_Keyboard_Button">H</button></td>
-        <td><button value="J" class="App_Keyboard_Button">J</button></td>
-        <td><button value="K" class="App_Keyboard_Button">K</button></td>
-        <td><button value="L" class="App_Keyboard_Button">L</button></td>
-        <td><button value="DEL" class="App_Keyboard_Button">⌫</button></td>
-      </tr>
-      <tr>
-        <td><button value="ENTER" class="App_Keyboard_Button">⏎</button></td>
-        <td><button value="Z" class="App_Keyboard_Button">Z</button></td>
-        <td><button value="X" class="App_Keyboard_Button">X</button></td>
-        <td><button value="C" class="App_Keyboard_Button">C</button></td>
-        <td><button value="V" class="App_Keyboard_Button">V</button></td>
-        <td><button value="B" class="App_Keyboard_Button">B</button></td>
-        <td><button value="N" class="App_Keyboard_Button">N</button></td>
-        <td><button value="M" class="App_Keyboard_Button">M</button></td>
-      </tr>
+    <table className="App_Keyboard">
+      <tbody>
+        <tr>
+          <td><button value="Q" className="App_Keyboard_Button">Q</button></td>
+          <td><button value="W" className="App_Keyboard_Button">W</button></td>
+          <td><button value="E" className="App_Keyboard_Button">E</button></td>
+          <td><button value="R" className="App_Keyboard_Button">R</button></td>
+          <td><button value="T" className="App_Keyboard_Button">T</button></td>
+          <td><button value="Y" className="App_Keyboard_Button">Y</button></td>
+          <td><button value="U" className="App_Keyboard_Button">U</button></td>
+          <td><button value="I" className="App_Keyboard_Button">I</button></td>
+          <td><button value="O" className="App_Keyboard_Button">O</button></td>
+          <td><button value="P" className="App_Keyboard_Button">P</button></td>
+        </tr>
+        <tr>
+          <td><button value="A" className="App_Keyboard_Button">A</button></td>
+          <td><button value="S" className="App_Keyboard_Button">S</button></td>
+          <td><button value="D" className="App_Keyboard_Button">D</button></td>
+          <td><button value="F" className="App_Keyboard_Button">F</button></td>
+          <td><button value="G" className="App_Keyboard_Button">G</button></td>
+          <td><button value="H" className="App_Keyboard_Button">H</button></td>
+          <td><button value="J" className="App_Keyboard_Button">J</button></td>
+          <td><button value="K" className="App_Keyboard_Button">K</button></td>
+          <td><button value="L" className="App_Keyboard_Button">L</button></td>
+          <td><button value="DEL" className="App_Keyboard_Button">⌫</button></td>
+        </tr>
+        <tr>
+          <td><button value="ENTER" className="App_Keyboard_Button">⏎</button></td>
+          <td><button value="Z" className="App_Keyboard_Button">Z</button></td>
+          <td><button value="X" className="App_Keyboard_Button">X</button></td>
+          <td><button value="C" className="App_Keyboard_Button">C</button></td>
+          <td><button value="V" className="App_Keyboard_Button">V</button></td>
+          <td><button value="B" className="App_Keyboard_Button">B</button></td>
+          <td><button value="N" className="App_Keyboard_Button">N</button></td>
+          <td><button value="M" className="App_Keyboard_Button">M</button></td>
+        </tr>
+      </tbody>
     </table>
   );
 }
