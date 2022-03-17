d238d15dd75a06f22b698c28be62d7ef05caf6e8
Emma Young <emma.maloney@bbc.co.uk>
2022-03-17 17:10:47 +0000

    feat: render grid based on current state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import Grid from './components/Grid';
 import Keyboard from './components/Keyboard';
 
 function App() {
+  const initialGrid = ['.....','.....','.....','.....','.....'];
+  const [guessedWords, setGuessedWords] = useState(initialGrid);
+
   const keyPressedHandler = (event) => {
-    console.log('key pressed!', event.target.id);
+    const keyPressed = event.target.id;
+
+    console.log('key pressed', keyPressed);
+    console.log('guess words', guessedWords);
   }
 
   return (
     <div className="App">
       <h1>Wordle React</h1>
-      <Grid />
+      <Grid guesses={guessedWords} />
       <Keyboard keyPressedEvent={keyPressedHandler} />
     </div>
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,42 +1,21 @@
-const Grid = () => {
+const Grid = ({guesses}) => {
   return (
     <table className="App_Grid">
       <tbody>
-        <tr id="guess-1">
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr id="guess-2">
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr id="guess-3">
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr id="guess-4">
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
-        <tr id="guess-5">
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>&nbsp;</td>
-        </tr>
+        {
+          guesses.map((guess, index) => {
+            const id = `guess-${index + 1}`;
+            const letters = guess.split('');
+
+            return <tr key={id} id={id}>
+              {
+                letters.map((letter) => {
+                  return <td>{letter !== '.' ? letter : ' '}</td>
+                })
+              }
+              </tr>
+          })
+        }
       </tbody>
     </table>
   );
